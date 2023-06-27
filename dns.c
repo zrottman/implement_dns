7443d6248f6f36fdabf36fdfba8cd1f929abd745
zrottman <zrottman@example.com>
2023-06-27 15:06:28 -0700

    Complete initial implementation of DNS Header struct

---
+++ dns.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+#define MAXQUESTION 100
+
+/* Structure definitions */
+struct DNSHeader {
+    int id;
+    int flags;
+    int num_questions;    // = 0
+    int num_answers;      // = 0
+    int num_authorities;  // = 0
+    int num_additionals;  // = 0
+};
+
+/* Function declarations */
+struct DNSHeader make_header(int id, int flags, int num_questions, int num_answers, int num_authorities, int num_additionals);
+
+int main()
+{
+
+    struct DNSHeader header;
+
+    header = make_header(5, 10, 1, 2, 3, 4);
+
+    printf("Header.id: %d\n", header.id);
+
+    return 0;
+}
+
+
+/* Make DNS header from query components */
+struct DNSHeader make_header(int id, int flags, int num_questions, int num_answers, int num_authorities, int num_additionals)
+{
+    struct DNSHeader header;
+
+    header.id               = id;
+    header.flags            = flags;
+    header.num_questions    = num_questions   ? num_questions   : 0;
+    header.num_answers      = num_answers     ? num_answers     : 0;
+    header.num_authorities  = num_authorities ? num_authorities : 0;
+    header.num_additionals  = num_additionals ? num_additionals : 0;
+
+    return header;
+}
