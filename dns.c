c20e965806f074b5e9967083dba55c5973250cbe
zrottman <zrottman@example.com>
2023-06-28 10:13:54 -0700

    Complete initial implementation of DNSQuestion struct

--- dns.c
+++ dns.c
@@ -4,24 +4,24 @@
 
 /* Structure definitions */
 struct DNSHeader {
-    int id;
-    int flags;
-    int num_questions;    // = 0
-    int num_answers;      // = 0
-    int num_authorities;  // = 0
-    int num_additionals;  // = 0
+    int  id;
+    int  flags;
+    int  num_questions;    // = 0
+    int  num_answers;      // = 0
+    int  num_authorities;  // = 0
+    int  num_additionals;  // = 0
 };
 
 struct DNSQuestion {
-    int name[];
-    int type;
-    int class;
+    char name[MAXQUESTION];
+    int  type;
+    int  class;
 };
 
 /* Function declarations */
 struct DNSHeader make_header(int id, int flags, int num_questions, int num_answers, int num_authorities, int num_additionals);
 
-struct DNSQuestion make_question(int name[], int type, int class, int lim);
+struct DNSQuestion make_question(char name[], int type, int class, int lim);
 
 int main()
 {
@@ -30,7 +30,7 @@ int main()
     struct DNSQuestion question;
 
     header = make_header(5, 10, 1, 2, 3, 4);
-    question = make_question("google", 5, 10, MAXQUESTION);
+    question = make_question("google\0", 5, 10, MAXQUESTION);
 
     printf("Header.id: %d\n", header.id);
     printf("Question.type: %d\n", question.type);
@@ -55,11 +55,14 @@ struct DNSHeader make_header(int id, int flags, int num_questions, int num_answe
 }
 
 /* Make DNS question */
-struct DNSQuestion make_question(int name[], int type, int class, int lim)
+struct DNSQuestion make_question(char name[], int type, int class, int lim)
 {
     struct DNSQuestion question;
+    int i;
 
-    question.name  = name[];
+    for (i=0; i<lim-1 && name[i] != '\0'; ++i) { question.name[i] = name[i]; }
     question.type  = type;
     question.class = class;
+
+    return question;
 }
